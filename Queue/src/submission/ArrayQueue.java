6ede6e9f5758b408178572e6115e294fa3c1a875
Kaycie Lynn O'Donnell <kodonnell@drew.edu>
2016-12-28 23:22:55 -0500

    my fixes

--- Queue/src/submission/ArrayQueue.java
+++ Queue/src/submission/ArrayQueue.java
@@ -5,34 +5,48 @@ import adt.Queue;
 public class ArrayQueue<T> implements Queue<T> {
 	private T[] data;
 	private int head, tail;
+
 	
 	@Override
 	public void enqueue(T newEntry) {
-		// TODO Auto-generated method stub
-		
+		tail = (tail+1) % data.length;
+		data[tail] = newEntry;
+		//will it expand automatically or just replace the same tail?
 	}
 
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()){
+			return null;
+		}
+		else { //make sure this works
+			T temp = data[head];
+			data[head] = null;
+			head = (head + 1) % data.length;
+			return temp;
+		}
+		//return null; //change this return - only return null if it's empty
 	}
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		return data[head];
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		//if the head is empty, the whole thing is empty
+		return data[head]==null;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		while (!isEmpty()) {
+			dequeue();
+		} //hopefully this will work
 		
 	}
 	
@@ -47,5 +61,7 @@ public class ArrayQueue<T> implements Queue<T> {
 		s+= "\n";
 		return s;
 	}
-
+public static void main(String[]args){
+	System.out.println("Hello");
+}
 }
